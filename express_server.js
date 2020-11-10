d1bb14a958c96d6c9f03d4a9174de889a6a0ac97
Erica <ericasu33@gmail.com>
2020-11-10 17:15:11 +0000

    added URL validation to verify if URL is valid, if invalid reroute them to a page where they can choose to go back to create new url or the url index. Also if user input randomness in the browser to give them a 404 page

--- express_server.js
+++ express_server.js
@@ -8,8 +8,20 @@ const generateRandomString = () => {
   return [...Array(6)].reduce(a => a + randomChar[~~(Math.random() * randomChar.length)],'');
 };
 
+const isUrl = str => {
+  const regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if (regexp.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+app.use(express.static("views"));
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -28,6 +40,14 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
+app.get('/urls/invalidURL', (req, res) => {
+  res.render('error_invalidURL');
+});
+
+app.get('/urls/404', (req, res) => {
+  res.render('error_404');
+});
+
 app.get('/urls', (req, res) => {
   const templateVar = { urls: urlDatabase };
   res.render('urls_index', templateVar);
@@ -39,20 +59,30 @@ app.get('/urls/new', (req, res) => {
 
 app.get('/urls/:shortURL', (req, res) => {
   const templateVar = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
-  res.render('urls_show', templateVar);
+  if (templateVar.longURL === undefined) {
+    res.status(404).render('error_404', templateVar);
+    // res.render('error_404', templateVar);   //-> why we don't need /urls/ here?
+  } else {
+    res.render('urls_show', templateVar);
+  }
 });
 
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
-  
+
   if (longURL.startsWith('http')) {
     urlDatabase[shortURL] = longURL;
   } else {
     urlDatabase[shortURL] = `http://${longURL}`;
   }
 
-  res.redirect(`/urls/${shortURL}`);
+  if (!isUrl(urlDatabase[shortURL])) {
+    res.redirect('/urls/invalidURL');
+  } else {
+    res.redirect(`/urls/${shortURL}`);
+  }
+
 });
 
 
