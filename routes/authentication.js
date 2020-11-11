bd70f4fac0de9120232a48287f85eed4ffb94abe
Erica <ericasu33@gmail.com>
2020-11-11 05:43:45 +0000

    added registration route handler and modifed what is being tracked for cookies -> user_id

--- routes/authentication.js
+++ routes/authentication.js
@@ -3,25 +3,52 @@ const app = module.exports = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+const { generateRandomString, users } = require('../helper');
+
 app.set('view engine', 'ejs');
 app.use(express.static("views"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
-//===========
+
+//=============
+//   Register
+//=============
+app.get('/register', (req, res) => {
+  const templateVar = {
+    user: users[req.cookies.user_id],
+  };
+
+  res.render('registration', templateVar);
+});
+
+app.post('/register', (req, res) => {
+  const id = generateRandomString();
+  users[id] = {
+    id,
+    email: req.body.email,
+    password: req.body.password
+  };
+  res.cookie('user_id', id);
+  res.redirect('/urls');
+
+  console.log(users);
+
+});
+
+//=============
 //   LOGIN
-//===========
+//=============
 
 app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username);
   res.redirect('/urls');
 });
 
-//===========
+//=============
 //   LOGOUT
-//===========
+//=============
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
