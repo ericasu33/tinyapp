3da8ecfda309ca94f80e25c5fb16b6dc7775605f
Erica <ericasu33@gmail.com>
2020-11-12 19:20:28 +0000

    added param database for isURL, and modified urlforuser to include date

--- helper.js
+++ helper.js
@@ -3,6 +3,7 @@ const generateRandomString = () => {
   return [...Array(6)].reduce(a => a + randomChar[~~(Math.random() * randomChar.length)],'');
 };
 
+//verify if URL is valid (not 100% false proof)
 const isUrl = str => {
   const regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
   if (regexp.test(str)) {
@@ -19,26 +20,30 @@ const urlDatabase = {
 
 const userDb = {};
 
-const registerUser = (id, email, password) => {
+const registerUser = (id, email, password, userDatabase) => {
   const user = { id, email, password };
-  return userDb[id] = user;
+  return userDatabase[id] = user;
 };
 
-const isUser = email => {
+const isUser = (email, userDatabase) => {
   let registered;
-  for (const id in userDb) {
-    if (userDb[id].email === email) {
-      registered = userDb[id];
+  for (const id in userDatabase) {
+    if (userDatabase[id].email === email) {
+      registered = userDatabase[id];
     }
   }
   return registered;
 };
 
-const urlsForUser = userID => {
+const urlsForUser = (userID, userDatabase) => {
   let urlDbByUser = {};
+
   for (const shortURL in urlDatabase) {
-    if (userDb[userID].id === urlDatabase[shortURL].userID) {
-      urlDbByUser[shortURL] = urlDatabase[shortURL].longURL;
+    const longURL = urlDatabase[shortURL].longURL;
+    const date = urlDatabase[shortURL].date;
+
+    if (userDatabase[userID].id === urlDatabase[shortURL].userID) {
+      urlDbByUser[shortURL] = { longURL, date };
     }
   }
   return urlDbByUser;
